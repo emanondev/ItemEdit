dfbf5990750f4f6996621e19f94ceaa30d96ed12
emanon <emanon_dev@hotmail.it>
2024-03-15 11:26:36 +0100

    + TrimPattern and TrimMaterial now rely on Registry instead of enumerated values

--- src/main/java/emanondev/itemedit/aliases/TrimPatternAliases.java
+++ src/main/java/emanondev/itemedit/aliases/TrimPatternAliases.java
@@ -1,45 +1,14 @@
 package emanondev.itemedit.aliases;
 
+import org.bukkit.Registry;
 import org.bukkit.inventory.meta.trim.TrimPattern;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 
-public class TrimPatternAliases extends AliasSet<TrimPattern> {
+public class TrimPatternAliases extends RegistryAliasSet<TrimPattern> {
     public TrimPatternAliases() {
-        super("trim_pattern");
-        registerValue(TrimPattern.COAST);
-        registerValue(TrimPattern.DUNE);
-        registerValue(TrimPattern.EYE);
-        registerValue(TrimPattern.RIB);
-        registerValue(TrimPattern.HOST);
-        registerValue(TrimPattern.RAISER);
-        registerValue(TrimPattern.SENTRY);
-        registerValue(TrimPattern.SHAPER);
-        registerValue(TrimPattern.SILENCE);
-        registerValue(TrimPattern.SPIRE);
-        registerValue(TrimPattern.SNOUT);
-        registerValue(TrimPattern.TIDE);
-        registerValue(TrimPattern.VEX);
-        registerValue(TrimPattern.WARD);
-        registerValue(TrimPattern.WILD);
-        registerValue(TrimPattern.WAYFINDER);
-    }
-
-    @Override
-    public String getName(TrimPattern value) {
-        return value.getKey().toString();
-    }
-
-    public void registerValue(TrimPattern pattern){
-        values.add(pattern);
-    }
-
-    private final HashSet<TrimPattern> values = new HashSet<>();
-
-    @Override
-    public Collection<TrimPattern> getValues() {
-        return Collections.unmodifiableCollection(values);
+        super("trim_pattern", Registry.TRIM_PATTERN);
     }
 }
