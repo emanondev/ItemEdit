08de8d13cb8a2ebe809b3947ef8e976c45871d74
emanon <emanon_dev@hotmail.it>
2024-03-15 11:37:38 +0100

    GoatHorn Sound now future version friendly

--- src/main/java/emanondev/itemedit/aliases/GoatHornSoundAliases.java
+++ src/main/java/emanondev/itemedit/aliases/GoatHornSoundAliases.java
@@ -1,24 +1,36 @@
 package emanondev.itemedit.aliases;
 
 import org.bukkit.MusicInstrument;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
 
 import java.util.Collection;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class GoatHornSoundAliases extends AliasSet<MusicInstrument> {
 
     public GoatHornSoundAliases() {
         super("goat_horn_sound");
-        MusicInstrument.values(); //force load the class or throw an exception if absent
+        try {//force load the class or throw an exception if absent
+            Registry.INSTRUMENT.stream().collect(Collectors.toList());
+        }catch (Throwable t){
+            MusicInstrument.values();
+        }
     }
 
     @Override
     public String getName(MusicInstrument type) {
-        return type.getKey().getKey().toLowerCase(Locale.ENGLISH);
+        return type.getKey().getNamespace().equals(NamespacedKey.MINECRAFT)?
+                type.getKey().getKey():type.getKey().toString();
     }
 
     @Override
     public Collection<MusicInstrument> getValues() {
-        return MusicInstrument.values();
+        try {//force load the class or throw an exception if absent
+            return Registry.INSTRUMENT.stream().collect(Collectors.toList());
+        }catch (Throwable t){
+            return MusicInstrument.values();
+        }
     }
 }
