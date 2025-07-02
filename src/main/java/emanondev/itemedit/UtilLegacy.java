f3d477068f552b73851b1e48726e2c973d43a410
timothe.rosaz <jamailun@laposte.net>
2025-07-02 20:55:10 +0200

    Effect duration can be written as double or 'instant'

--- src/main/java/emanondev/itemedit/UtilLegacy.java
+++ src/main/java/emanondev/itemedit/UtilLegacy.java
@@ -6,7 +6,7 @@ import org.jetbrains.annotations.NotNull;
 public final class UtilLegacy {
 
     /**
-     * This method accepts <code>infinite</code>,<code>∞</code> or any number as valid input.<br>
+     * This method accepts <code>infinite</code>,<code>∞</code>, <code>instant</code> or any number as valid input.<br>
      * On versions before 1.19.4, infinite is turned to <code>Integer.MAX_VALUE</code>.<br><br>
      * In API versions 1.19.3 and earlier, there is no infinite value for PotionEffects.<br>
      * In API versions 1.19.4 and later, -1 is the value for infinite duration.
@@ -15,16 +15,39 @@ public final class UtilLegacy {
      * @throws NumberFormatException When value cannot be interpreted to a valid value
      */
     public static int readPotionEffectDurationSecondsToTicks(@NotNull String value) {
-        int duration = (value.equalsIgnoreCase("infinite")
-                || (value.equalsIgnoreCase("∞"))) ?
-                -1 : (Integer.parseInt(value));
-        if (duration >= 0) {
-            duration *= 20; //to ticks
-        } else if (!VersionUtils.isVersionAfter(1, 19, 4)) {
-            duration = Integer.MAX_VALUE;
-        } else {
-            duration = -1;
+        double durationSecs = readPotionDurationSeconds(value);
+
+        // Zero should be 1 tick.
+        if(durationSecs == 0) {
+            return 1;
         }
-        return duration;
+
+        // Seconds to ticks : *20
+        if(durationSecs > 0) {
+            return (int) (durationSecs * 20);
+        }
+
+        // Infinite duration
+        return VersionUtils.isVersionAfter(1, 19, 4) ? Integer.MAX_VALUE : -1;
+    }
+
+    /**
+     * This method accepts <code>infinite</code>,<code>∞</code>, <code>instant</code> or any number as valid input.<br>
+     * It transforms a string into a floating-point value in seconds.
+     * @param value The raw value to be interpreted, values below <code>0</code> are treated as infinite.
+     * @return a duration, in seconds.
+     * @throws NumberFormatException When value cannot be interpreted to a valid double.
+     */
+    private static double readPotionDurationSeconds(@NotNull String value) {
+        // Infinite duration.
+        if(value.equalsIgnoreCase("infinite") || value.equalsIgnoreCase("∞")) {
+            return -1;
+        }
+        // zero-duration.
+        if(value.equalsIgnoreCase("instant")) {
+            return 0;
+        }
+        // Normal duration.
+        return Double.parseDouble(value);
     }
 }
